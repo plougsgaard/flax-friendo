2de599d3238cd1f1c375e076c83433b88e731761
plougsgaard <thomas@plougsgaard.dk>
2018-03-23 21:43:37 +0100

    Write brilliant programme why not.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('hellow2')
+const fetch = require('node-fetch')
+
+const URL = 'http://localhost:8003'
+const testProgram = 'def f(): Int = 43333'
+
+const myProgramme = async () => {
+  const res = await fetch(URL, { method: 'post', body: testProgram })
+  const json = await res.json()
+  console.log('wooo', json)
+}
+
+
+myProgramme()
