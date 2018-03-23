52ab15c3b7ae3cbac8ae2d390062a94e080ef544
plougsgaard <thomas@plougsgaard.dk>
2018-03-23 21:55:39 +0100

    Make awesome websocket server that server flax stuff.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 const fetch = require('node-fetch')
+const Koa = require('koa')
+const websocketify = require('koa-websocket')
 
 const URL = 'http://localhost:8003'
-const testProgram = 'def f(): Int = 43333'
 
-const myProgramme = async () => {
-  const res = await fetch(URL, { method: 'post', body: testProgram })
+const myProgramme = async (program) => {
+  const res = await fetch(URL, { method: 'post', body: program })
   const json = await res.json()
-  console.log('wooo', json)
+  return json
 }
 
+const server = websocketify(new Koa())
 
-myProgramme()
+server.ws.use(async ctx => {
+  ctx.websocket.on('message', async message => {
+    const result = await myProgramme(message)
+    ctx.websocket.send(JSON.stringify(result))
+  })
+})
+
+server.listen(8004)
